c916a803b9ae669736eedea9253fc1472d0afc2a
Nwogeh Chinenyenwa Ogbonna <johnchinenye6@gmail.com>
2023-07-05 11:39:05 +0100

    Updated

--- modules/RDS/rds.tf
+++ modules/RDS/rds.tf
@@ -1,7 +1,7 @@
 # This section will create the subnet group for the RDS  instance using the private subnet
 resource "aws_db_subnet_group" "ACS-rds" {
   name       = "acs-rds"
-  subnet_ids = var.private_subnets
+  subnet_ids = ["subnet-04c281974cefc37a8"]
 
   tags = merge(
     var.tags,
