49ef675e25f007f804f4b5dad13e8d2af8d7a511
Nwogeh Chinenyenwa Ogbonna <johnchinenye6@gmail.com>
2023-07-07 15:37:48 +0100

    Update web.sh

--- AMI/web.sh
+++ AMI/web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-sudo yum install -y dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+sudo yum install -y dnf-utils http://rpms.remirepo.net/enterprise/remi-release-9.rpm
 sudo yum install -y mysql wget vim telnet htop git python3 net-tools 
 sudo systemctl start chronyd
 sudo systemctl enable chronyd
